29ed2bc3c960b10bc980e6fd4a3c52f0ab994e4c
Henning Sudbrock <github@hsudbrock.de>
2021-02-06 18:59:27 +0100

    Add workspace reading from a file-based portage maven artifact registry.

--- src/main/java/de/hsudbrock/portagemavenworkspacereader/PortageWorkspaceReader.java
+++ src/main/java/de/hsudbrock/portagemavenworkspacereader/PortageWorkspaceReader.java
@@ -1,6 +1,11 @@
 package de.hsudbrock.portagemavenworkspacereader;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Named;
@@ -13,6 +18,8 @@ import org.eclipse.aether.repository.WorkspaceRepository;
 @Named("ide")
 @Singleton
 public class PortageWorkspaceReader implements WorkspaceReader {
+	
+	private static final Path ARTIFACT_REGISTRY_DIRECTORY = Paths.get("/usr/share/portage-maven-artifact-registry");
 	
 	private final WorkspaceRepository workspaceRepository;
 	
@@ -28,13 +35,35 @@ public class PortageWorkspaceReader implements WorkspaceReader {
 	@Override
 	public File findArtifact(Artifact artifact) {
 		System.out.println("Trying to find artifact: " + artifact);
-		return null;
+		Path artifactLocatorPath = getArtifactLocatorPath(artifact);
+		if (Files.exists(artifactLocatorPath)) {
+			String artifactLocation;
+			try {
+				artifactLocation = Files.readAllLines(artifactLocatorPath).get(0);
+			} catch (IOException e) {
+				System.out.println("Could not read artifact locator file at: " + artifactLocatorPath);	
+				return null;
+			}
+			System.out.println("Found artifact location for " + artifact + " at " + artifactLocation);
+			return new File(artifactLocation);
+		} else {
+			System.out.println("Could not find artifact location for: " + artifact);
+			return null;
+		}
 	}
 
 	@Override
 	public List<String> findVersions(Artifact artifact) {
 		System.out.println("Trying to find versions: " + artifact);
-		return null;
+		System.out.println("Not yet implemented");
+		return new ArrayList<>();
+	}
+	
+	private Path getArtifactLocatorPath(Artifact artifact) {
+		return ARTIFACT_REGISTRY_DIRECTORY
+				.resolve(artifact.getGroupId())
+				.resolve(artifact.getArtifactId())
+				.resolve(artifact.getVersion() + "-" + artifact.getExtension() + ".location");
 	}
 
 }
